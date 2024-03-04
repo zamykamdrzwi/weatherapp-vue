f5984516fdb217542a8b9b2ca74fb9c257da5dc5
unknown <rafdaw1337@wp.pl>
2024-03-04 10:40:42 +0100

    sass test 2

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="hello">Header</div>
+  <div class="text-primary mt-8">Header</div>
+  <div class="btn btn-primary">cokolwiek</div>
 </template>
 
 <style lang="scss" scoped>
-@import '../../assets/style/test.scss';
-// $color: red;
-.hello {
-  color: $color;
-}
+@import '../../assets/style/bootstrap.scss';
+// .hello {
+//   color: $primary;
+// }
 </style>
