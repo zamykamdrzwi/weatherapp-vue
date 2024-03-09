60b20eb133c98e1bbc6f16ebb093397c5aab7a39
unknown <rafdaw1337@wp.pl>
2024-03-09 12:13:10 +0100

    fixed loops in ForecastOutput.vue

--- src/components/weather/ForecastOutput.vue
+++ src/components/weather/ForecastOutput.vue
@@ -1,63 +1,13 @@
 <template>
   <ul v-if="forecastList && forecastList.list">
-    <li>
-      <div>Day 1</div>
+    <li v-for="item in forecastWeek" :key="item">
       <ul>
-        <li v-for="day in forecastWeek.day1" :key="day">
-          {{ day.dt_txt }}
-        </li>
-      </ul>
-    </li>
-    <li>
-      <div>Day 2</div>
-      <ul>
-        <li v-for="day in forecastWeek.day2" :key="day">
-          {{ day.dt_txt }}
-        </li>
-      </ul>
-    </li>
-    <li>
-      <div>Day 3</div>
-      <ul>
-        <li v-for="day in forecastWeek.day3" :key="day">
-          {{ day.dt_txt }}
-        </li>
-      </ul>
-    </li>
-    <li>
-      <div>Day 4</div>
-      <ul>
-        <li v-for="day in forecastWeek.day4" :key="day">
-          {{ day.dt_txt }}
-        </li>
-      </ul>
-    </li>
-    <li>
-      <div>Day 5</div>
-      <ul>
-        <li v-for="day in forecastWeek.day5" :key="day">
-          {{ day.dt_txt }}
-        </li>
-      </ul>
-    </li>
-    <li>
-      <div>Day 6</div>
-      <ul>
-        <li v-for="day in forecastWeek.day6" :key="day">
-          {{ day.dt_txt }}
-        </li>
-      </ul>
-    </li>
-    <li>
-      <div>Day 7</div>
-      <ul>
-        <li v-for="day in forecastWeek.day7" :key="day">
-          {{ day.dt_txt }}
+        <li v-for="x in item" :key="x">
+          {{ x.dt_txt }}
         </li>
       </ul>
     </li>
   </ul>
-  <div v-else>cokwliek</div>
 </template>
 
 <script>
@@ -65,15 +15,7 @@ export default {
   props: ['showUnit'],
   data() {
     return {
-      forecastWeek: {
-        day1: [],
-        day2: [],
-        day3: [],
-        day4: [],
-        day5: [],
-        day6: [],
-        day7: []
-      }
+      forecastWeek: []
     };
   },
   computed: {
@@ -90,42 +32,23 @@ export default {
     showForecast() {
       console.log(this.forecastList);
       let currentDate = this.forecastList.list[0].dt_txt.substring(0, 10);
-      let day = 1;
+      let day = 0;
+
       this.forecastList.list.forEach(item => {
         if(item.dt_txt.substring(0, 10) === currentDate) {
-          switch (day) {
-            case 1:
-              this.forecastWeek.day1.push(item);
-              break;
-            case 2:
-              this.forecastWeek.day2.push(item);
-              break;
-            case 3:
-              this.forecastWeek.day3.push(item);
-              break;
-            case 4:
-              this.forecastWeek.day4.push(item);
-              break;
-            case 5:
-              this.forecastWeek.day5.push(item);
-              break;
-            case 6:
-              this.forecastWeek.day6.push(item);
-              break;
-            case 7:
-              this.forecastWeek.day7.push(item);
-              break;
-            default:
-              console.log('Error with switch!');
-              break;
-          }
-        }
-        else {
+          this.forecastWeek[day] = [];
+          
+          this.forecastList.list.forEach(x => {
+            if(x.dt_txt.substring(0, 10) === currentDate) {
+              this.forecastWeek[day].push(x)
+            }
+          });
+        } else {
           currentDate = item.dt_txt.substring(0, 10);
-          day++;
-          console.log(day)
+          day ++;
         }
       });
+
       console.log(this.forecastWeek)
     }
   }
