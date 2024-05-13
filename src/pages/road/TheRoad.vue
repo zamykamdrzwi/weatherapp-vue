2806d17c52c2378082210bfc14e1da0813806122
unknown <rafdaw1337@wp.pl>
2024-05-13 13:37:58 +0200

    added temporary img on route

--- src/pages/road/TheRoad.vue
+++ src/pages/road/TheRoad.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>The Road</div>
+  <div class="tempImg d-flex justify-content-center align-items-center">
+    <img id="img-rel" src="../../assets/bg-route2.png" alt="web under construction">
+  </div>
 </template>
+
+<style scoped lang="scss">
+#img-rel {
+  max-width: 100%;
+  height: auto;
+}
+</style>
