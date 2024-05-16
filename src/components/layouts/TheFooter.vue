20304620eed140e47ab5cfe2d7ffc4e99e91d11b
unknown <rafdaw1337@wp.pl>
2024-05-16 13:26:38 +0200

    added footer

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -1,14 +1,36 @@
 <template>
-  <div class="mt-auto fixed-bottom">
-    <footer class="footer bg-light">
-      <div class="container pt-3 pb-3">footer</div>
-    </footer>
-  </div>
+<!--  <div class="mt-auto fixed-bottom">-->
+<!--    <footer class="footer bg-light">-->
+<!--      <div class="container pt-3 pb-3">footer</div>-->
+<!--    </footer>-->
+<!--  </div>-->
+  <footer class="footer">
+    <div class="container d-flex align-items-center justify-content-center">
+      <span class="text-muted">
+        Website created by Rafał Dawidczyk
+      </span>
+      <span class="fs-4">&nbsp;&nbsp;&nbsp;
+        <a href="https://github.com/zamykamdrzwi/weatherapp-vue" target="_blank">
+          <i class="bi bi-github"></i>
+        </a>
+      </span>
+      <span class="fs-4">&nbsp;
+        <a href="https://www.linkedin.com/in/rafa%C5%82-dawidczyk-769427243/" target="_blank">
+          <i class="bi bi-linkedin"></i>
+        </a>
+      </span>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @import '../../assets/style/style.scss';
 .footer {
-
+  padding: 20px 0;
+  text-align: center;
+  bottom: 0;
+  width: 100%;
 }
 </style>
+<script setup lang="ts">
+</script>
